1ad936173a147b2b32d9268689e2dc7d3a851196
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-12-05 21:01:10 -0500

    contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,9 +5,9 @@ const About = () => {
         <div className='container aboutPage'>
             <div className='textArea'>
                 <h1>About Me</h1>
-                <p>I'm a student at Columbia Coding Bootcamp. Learning to become a Full Stack Developer. I'm very curious and ambitious front-end developer looking for a role in tech.</p>
-                <p>I'm looking for opportunity to work the latest technologies to tackle on challenges and diverse projects. As a lifelong student, I am open minded into learning new technologies and adapting to it. I am confident in my abilities, constantly working on improving myself, naturally curious, and work well in team environments.</p>
-                <p>If I had to summarize me in one sentence that would be tech enthusiast, sport fanatic, foodie, gamer, and a explorer.</p>
+                <p>I'm a student at Columbia Coding Bootcamp. Learning to become a Full Stack Developer. I'm very curious and ambitious front-end developer looking for a role in an established IT company.</p>
+                <p>I'm looking for opportunities to work with the latest technologies to tackle on challenges and diverse projects. As a lifelong student, I am open minded into learning new technologies and adapting to it. I am confident in my abilities, constantly working on improving myself, naturally curious, and work well in team environments.</p>
+                <p>If I had to summarize me in one sentence that would be tech enthusiast, sport fanatic, foodie, gamer, and an explorer.</p>
             </div>
         </div>
     )
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,50 @@
 import './index.scss'
 
-const Contact = () => "Hello"
+const Contact = () => {
+    return(
+    <div className='container contactPage'>
+        <div className='textArea'>
+            <h1>Contact Me</h1>
+            <p>I am interested in working with other developers and always open to new opporunities for projects. If for any reason you need to contact me please fill out the form below. </p>
+            <div className="contactForm">
+            <form>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+    </div>    
+    )
+}
 
 
 
